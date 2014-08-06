77bac66acc47ca11436982482d1a9bfc82320206
石俊玮 <616429216>
2014-08-06 16:05:14 +0800

    把函数调用放到controller里面

--- app/scripts/controllers/signUp.js
+++ app/scripts/controllers/signUp.js
@@ -1,15 +1,8 @@
 'use strict';
 
 app.controller('signUpController', function($scope, NewUser){
-<<<<<<< HEAD
 	$('#birthday').datepicker();
-=======
-
->>>>>>> 274ff49d94357ed608066b34c566be7958dbcc7d
     $scope.signUp = function(user){
         NewUser.signUp(user, $scope);
-    }
-   
-    	
-    	
+    } 	
 });
