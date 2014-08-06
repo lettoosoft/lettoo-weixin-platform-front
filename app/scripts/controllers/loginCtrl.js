65fdb5ca7bdc13a7b7b59f9884a462cdaeabfe2b
石俊玮 <616429216>
2014-08-06 15:39:29 +0800

    no message

--- app/scripts/controllers/loginCtrl.js
+++ app/scripts/controllers/loginCtrl.js
@@ -2,7 +2,6 @@
 
 app.controller('loginController', function($scope, User){
     $scope.message = '';
-
     $scope.login = function(user){
         User.login(user, $scope);
     }
--- app/scripts/controllers/signUp.js
+++ app/scripts/controllers/signUp.js
@@ -1,6 +1,7 @@
 'use strict';
 
 app.controller('signUpController', function($scope, NewUser){
+	$('#birthday').datepicker();
     $scope.signUp = function(user){
         NewUser.signUp(user, $scope);
     }
