274ff49d94357ed608066b34c566be7958dbcc7d
chenhanxiao <575090833@qq.com>
2014-08-06 15:35:55 +0800

    初步增加了表单验证

--- app/scripts/controllers/signUp.js
+++ app/scripts/controllers/signUp.js
@@ -1,7 +1,11 @@
 'use strict';
 
 app.controller('signUpController', function($scope, NewUser){
+
     $scope.signUp = function(user){
         NewUser.signUp(user, $scope);
     }
+   
+    	
+    	
 });
