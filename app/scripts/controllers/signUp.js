4632832781c3f3fc7431e8971206d25d1e075459
石俊玮 <616429216>
2014-08-06 12:21:14 +0800

    初步完成注册工作

---
+++ app/scripts/controllers/signUp.js
@@ -0,0 +1,7 @@
+'use strict';
+
+app.controller('signUpController', function($scope, NewUser){
+    $scope.signUp = function(user){
+        NewUser.signUp(user, $scope);
+    }
+});
---
+++ app/scripts/services/signUpService.js
@@ -0,0 +1,23 @@
+'use strict';
+
+app.factory('NewUser', ['$http', function ($http) {
+    var NewUser = function (data) {
+        angular.extend(this, data);
+    };
+
+    NewUser.signUp = function (user, scope) {
+        var url = 'http://121.40.126.220/api/v1/createuser/';
+
+        return $http.post(url, user).success(function (data) {
+            //IMPORTANT: You need to activate always_return_data in your ressource (see example)
+            user.id = data.id;
+            console.log(data);
+            scope.message = '成功';
+        }).error(function (data) {
+            console.log(data);
+            scope.message = data.error;
+        });
+    };
+
+    return NewUser;
+}]);
